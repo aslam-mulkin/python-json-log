1471ab68c456b9c44ec82df73739bdecb061392a
Aslam Mulkin <aslamsrq@gmail.com>
2023-11-21 22:18:24 +0700

    Create log_generator.py

---
+++ log_generator.py
@@ -0,0 +1,19 @@
+import json
+import random
+import time
+
+def generate_apache_log():
+    timestamp = time.strftime('%d/%b/%Y:%H:%M:%S %z')
+    ip_address = f'{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+
+    log_entry = f'{ip_address} - - [{timestamp}] "GET /" 200 - "{user_agent}"'
+    json_log = {'log_entry': log_entry}
+
+    # Print JSON log to stdout
+    print(json.dumps(json_log))
+
+if __name__ == '__main__':
+    while True:
+        generate_apache_log()
+        time.sleep(5)
